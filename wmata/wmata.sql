8c9db327126674a2fd78ed89905e0f3564838fd0
Jason Baumgartner <jason@pushshift.io>
2016-07-21 05:28:41 -0400

    Improved bus_data unique index

--- wmata/wmata.sql
+++ wmata/wmata.sql
@@ -9,7 +9,7 @@ CREATE TABLE bus_data (
 CREATE TABLE train_data (
     json jsonb
 );
-CREATE UNIQUE INDEX route_trip_vehicle_retrieved ON bus_data USING btree (((json ->> 'RouteID'::text)), ((json ->> 'TripID'::text)), ((json ->> 'VehicleID'::text)), (((json ->> 'retrieved_on'::text))::integer));
+CREATE UNIQUE INDEX route_trip_vehicle_datetime ON bus_data USING btree (((json ->> 'RouteID'::text)), ((json ->> 'TripID'::text)), ((json ->> 'VehicleID'::text)), (((json ->> 'DateTime'::text))::integer));
 CREATE UNIQUE INDEX train_retrieved ON train_data USING btree (((json ->> 'TrainId'::text)), (((json ->> 'retrieved_on'::text))::integer));
 
 
